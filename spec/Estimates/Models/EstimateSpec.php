b5e0ba72865efc935415662fb96ca627ca036791
W <wout@users.noreply.github.com>
2018-06-05 19:44:44 +0200

    Estimate validation and composer

--- spec/Estimates/Models/EstimateSpec.php
+++ spec/Estimates/Models/EstimateSpec.php
@@ -8,19 +8,33 @@ use Prophecy\Argument;
 
 class EstimateSpec extends ObjectBehavior {
 
+    function let() {
+      $this->beConstructedWith([
+        'details' => [
+          'email' => 'pddi@zwartopwit.be'
+        , 'telephone' => '0432654987'
+        ]
+      ]);
+    }
+
     // IsValid method
     function it_can_be_valid() {
       $this->isValid()->shouldBe( true );
     }
 
     function it_can_be_invalid() {
+      $this->beConstructedWith([]);
       $this->isValid()->shouldBe( false );
     }
 
 
     // Compose method
     function it_composes_an_email_from_given_form_data() {
-
+      $composed = $this->compose();
+      $composed->shouldContain( '------------------------------' );
+      $composed->shouldContain( 'Details' );
+      $composed->shouldContain( 'email: pddi@zwartopwit.be' );
+      $composed->shouldContain( 'telephone: 0432654987' );
     }
 
 
@@ -31,8 +45,20 @@ class EstimateSpec extends ObjectBehavior {
 
 
     // Store method
-    function it_stores_the_uplaoded_files_on_the_file_system() {
+    function it_stores_the_uploaded_files_on_the_file_system() {
 
+    }
+
+
+    // Errors methods
+    function it_returns_the_errors_array() {
+      $this->errors()->shouldBeArray();
+    }
+
+    function it_returns_an_array_with_errors() {
+      $this->beConstructedWith([]);
+      $this->isValid();
+      $this->errors()->shouldHaveCount( 2 );
     }
 
 
--- src/Estimates/Models/Estimate.php
+++ src/Estimates/Models/Estimate.php
@@ -2,10 +2,58 @@
 
 namespace Estimates\Models;
 
+use Illuminate\Support\Str;
+use Bulckens\AppTools\App;
+
 class Estimate {
 
-  public function isValid() {
+  protected $data;
+  protected $errors;
 
+  public function __construct( array $data ) {
+    $this->data = $data;
+  }
+
+
+  // Validate estimate data
+  public function isValid() {
+    $this->errors = [];
+
+    if ( ! isset( $this->data['details']['email'] )) {
+      $this->errors[] = 'E-mail adres is vereist';
+    }
+
+    if ( ! isset( $this->data['details']['telephone'] )) {
+      $this->errors[] = 'Telefoon is vereist';
+    }
+
+    return empty( $this->errors );
+  }
+
+
+  // Compose data into a readable text string
+  public function compose() {
+    $composed = [];
+
+    foreach ( $this->data as $section => $pairs ) {
+      $composed[] = "------------------------------";
+      $composed[] = Str::studly( $section );
+      $composed[] = "------------------------------";
+
+      foreach ( $pairs as $key => $value ) {
+        $composed[] = "$key: $value";
+      }
+
+      $composed[] = "\n";
+    }
+
+    return implode( "\n", $composed );
+  }
+
+
+  // Returns error
+  public function errors() {
+    return $this->errors ?: [];
   }
 
 }
