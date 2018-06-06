e16a166d5aabf2a3f20903bb6422336f30d91428
W <wout@users.noreply.github.com>
2018-06-06 16:08:00 +0200

    Debugging env

--- src/Estimates/Models/Estimate.php
+++ src/Estimates/Models/Estimate.php
@@ -67,6 +67,7 @@ class Estimate {
     // send mail
     $response = $mail->post();
 var_dump( $response );
+var_dump( \Bulckens\AppTools\App::env());
 die();
     // detect successful
     return !! $response->attr( 'success' );
